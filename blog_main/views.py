4ccd632c15aa0c2d242fe23f0432f54ff823cced
Kamate2025 <kamatentuyenabo184@gmail.com>
2025-12-29 22:45:22 +0300

    Posts by category fully implemented

--- blog_main/views.py
+++ blog_main/views.py
@@ -3,14 +3,11 @@ from blogs.models import Blog, Category
 
 
 
-def home(request):
-    categories = Category.objects.all()
-    
+def home(request):    
     featured_posts = Blog.objects.filter(is_featured=True).order_by('-updated_at')
     un_featured_posts = Blog.objects.filter(is_featured=False)
     
     context = {
-        "categories": categories,
         'featured_posts': featured_posts,
         'un_featured_posts': un_featured_posts,
     }
