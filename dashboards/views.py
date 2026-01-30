94572cb0e587feb69775beb877d61ddde4b981af
Kamate2025 <kamatentuyenabo184@gmail.com>
2026-01-30 23:19:57 +0300

    Done with crud operations for Users

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import get_object_or_404, render, redirect
+from blog_main.forms import RegistrationForm
 from blogs.models import Category, Blog
 from django.contrib.auth.decorators import login_required
-from .forms import BlogForm, CategoryForm
+from .forms import AddUser, BlogForm, CategoryForm
 from django.utils.text import slugify
+from django.contrib.auth.models import User
 
 
 @login_required(login_url='login')
@@ -99,3 +101,49 @@ def edit_post(request, pk):
         'blog': blog,
     }
     return render(request, 'dashboard/edit_post.html', context)
+
+
+def users(request):
+    users = User.objects.all()
+    context = {
+        'users': users,
+    }
+    return render(request, 'dashboard/users.html', context)
+
+
+def delete_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    user.delete()
+    return redirect('users')
+
+
+def edit_user(request, pk):
+    user = get_object_or_404(User, pk=pk)
+    if request.method == 'POST':
+        form = AddUser(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUser(instance=user)
+    context = {
+        'form': form,
+        'user': user,
+        }
+    return render(request, 'dashboard/edit_user.html', context)
+
+
+def add_user(request):
+    if request.method == 'POST':
+        form = AddUser(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('users')
+        else:
+            print(form.errors)
+    form = AddUser()
+    context = {
+        'form': form,
+    }
+    return render(request, 'dashboard/add_user.html', context)
